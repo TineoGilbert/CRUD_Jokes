e95d74c2258b51e55ce09cd5be01eefbe2ecb1b5
TineoGilbert <65523573+TineoGilbert@users.noreply.github.com>
2021-06-28 09:48:31 -0400

    CRUD JOKES .NET Core 5

---
+++ CRUD_Jokes/Models/Jokes.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUD_Jokes.Models
+{
+    public class Jokes
+    {
+        public int Id { get; set; }
+
+        public string JokeQuestion { get; set; }
+
+        public string JokeAnswer { get; set; }
+
+        public Jokes()
+        {
+
+        }
+    }
+}
